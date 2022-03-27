8374669f5988ce1ed4e3ee979c7ef62d01ddb667
dbornilla <78960805+dbornilla@users.noreply.github.com>
2022-03-27 21:55:43 +0800

    About us activity

--- app/src/main/java/com/example/workouttimerapplication/MainActivity.java
+++ app/src/main/java/com/example/workouttimerapplication/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.workouttimerapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button about_bnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        about_bnt = findViewById(R.id.aboutButton);
+        about_bnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aboutActivity();
+            }
+        });
     }
+
+    public void aboutActivity(){
+        Intent intent = new Intent(this,About.class);
+        startActivity(intent);
+    }
+
 }
